f4a5df344521ec0a112f8f47584126dd48bac573
michael19 <rekmixa@gmail.com>
2021-05-01 14:35:16 +0500

    change Dockerfile & add example swoole coroutine

--- cli/index.php
+++ cli/index.php
@@ -1,5 +1,24 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+Co\run(function() {
+  go(function() {
+    while (true) {
+      sleep(1);
+      echo "Done 1\n";
+    }
+  });
 
-echo "Test\n";
+  go(function() {
+    while (true) {
+      sleep(1);
+      echo "Done 2\n";
+    }
+});
+
+  go(function() {
+    while (true) {
+      sleep(1);
+      echo "Done 3\n";
+    }
+  });
+});
